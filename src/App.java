a93966d4a2ca9bcf70d1472f5579536a0e4dffdf
DejavuNerdPro <121812138+DejavuNerdPro@users.noreply.github.com>
2023-05-29 23:15:25 +0630

    Finish

--- src/App.java
+++ src/App.java
@@ -6,10 +6,20 @@ public class App {
         AuthenticHuman authenticHuman = new AuthenticHuman();
         authenticHuman.improveTalency();
 
+        System.out.println("------");
+
         // The add-on class
         Human talentedHuman = new TalentedHuman(authenticHuman);
         // or Hman talentedHuman=new TalentedHuman(new AuthenticHuman());
         talentedHuman.improveTalency();
+
+        System.out.println("-------");
+
+        // The add-on class 2
+        Human brillientHuman = new BrillientHuman(new TalentedHuman(new AuthenticHuman()));
+        brillientHuman.improveTalency();
+
+        System.out.println("-------");
 
     }
 }
