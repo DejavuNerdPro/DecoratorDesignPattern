6f9e59e7d3aeb8eb5bffb8d4862cb8415e752abf
DejavuNerdPro <121812138+DejavuNerdPro@users.noreply.github.com>
2023-05-29 22:50:12 +0630

    The Original Class is added.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Decorator Design Pattern!");
+
+        // Authentic Human : the original class
+        AuthenticHuman authenticHuman = new AuthenticHuman();
+        authenticHuman.improveTalency();
+
     }
 }
---
+++ src/AuthenticHuman.java
@@ -0,0 +1,8 @@
+
+public class AuthenticHuman implements Human {
+
+    @Overried
+    public void improveTalency() {
+        System.out.println("Never Consider about improvement.");
+    }
+}
